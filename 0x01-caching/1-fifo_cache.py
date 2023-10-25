799dbc93a4fa3539bb1dd9841e268865ab4c292b
Krofah <amponsahkrofah@gmail.com>
2023-10-25 12:34:38 +0000

    task 0

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -19,22 +19,24 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """
         Cache a key-value pair
+        Args:
+            key: The key to cache
+            item: The value associated with the key
         """
-        if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[0]))
-                del self.cache_data[self.order[0]]
-                del self.order[0]
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                discarded_key = self.order.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
             self.order.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        Return the value linked to a given key, or None
+        Retrieve the value associated with a key
+        Args:
+            key: The key to retrieve the value for
+        Returns:
+            The value associated with the key, or None if the key doesn't exist
         """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        return self.cache_data.get(key, None)
